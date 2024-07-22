b24a73b3bf4df9f8032fad3a7e41e117a7091426
L-ama <lamaalrayees2006@gmail.com>
2024-07-22 19:43:17 +0300

    Update Question1.c

--- Question1.c
+++ Question1.c
@@ -1,27 +1,12 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <meta http-equiv="Content-Style-Type" content="text/css">
-  <title></title>
-  <meta name="Generator" content="Cocoa HTML Writer">
-  <meta name="CocoaVersion" content="2113.5">
-  <style type="text/css">
-    p.p1 {margin: 0.0px 0.0px 0.0px 0.0px; font: 12.0px Helvetica}
-  </style>
-</head>
-<body>
-<p class="p1">#include&lt;stdio.h&gt;</p>
-<p class="p1"><span class="Apple-converted-space"> </span>int main()</p>
-<p class="p1">{</p>
-<p class="p1"><span class="Apple-converted-space">    </span>float circumference, side_lenght,area;</p>
-<p class="p1"><span class="Apple-converted-space">    </span>printf("enter the side_lenght of the suqare:%f\n");</p>
-<p class="p1"><span class="Apple-converted-space">    </span>scanf("%f",&amp;side_lenght);</p>
-<p class="p1"><span class="Apple-converted-space">    </span>circumference= 4 * side_lenght;</p>
-<p class="p1"><span class="Apple-converted-space">    </span>area= side_lenght * side_lenght;</p>
-<p class="p1"><span class="Apple-converted-space">    </span>printf("the circumference of the suqare:%f\n", circumference);</p>
-<p class="p1"><span class="Apple-converted-space">    </span>printf("area of the suqare:%f\n", area);</p>
-<p class="p1"><span class="Apple-converted-space">    </span>return 0;</p>
-<p class="p1">}</p>
-</body>
-</html>
+#include<stdio.h>
+ int main()
+{
+    float circumference, side_lenght,area;
+    printf("enter the side_lenght of the suqare:%f\n");
+    scanf("%f",&side_lenght);
+    circumference= 4 * side_lenght;
+    area= side_lenght * side_lenght;
+    printf("the circumference of the suqare:%f\n", circumference);
+    printf("area of the suqare:%f\n", area);
+    return 0;
+}
